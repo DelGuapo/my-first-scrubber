2f4a5097923629fc682d48b6f8beb3c377e753c6
Nick <bitweaverllc@gmail.com>
2018-07-04 14:26:17 -0600

    demo class, import a config as class

---
+++ assets/myConfig.py
@@ -0,0 +1,14 @@
+class Config:
+    """
+        Init Function (constructor);
+    """
+    def __init__(self):
+        self.myDir = 'C:\/Users\/Nicholas Weaver'
+
+class OtherClass:
+    """
+        just a demo of another class/function
+    """
+    def fn(self, var):
+        return var
+

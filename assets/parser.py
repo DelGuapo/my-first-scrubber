13984d9c258d076d9bae5d8b0efad2ce4e79a2c4
Nick <bitweaverllc@gmail.com>
2018-07-04 14:52:52 -0600

    commenting and initial attempt of beautiful soup-ing

--- assets/parser.py
+++ assets/parser.py
@@ -1,8 +1,9 @@
-from bs4 import BeautifulSoup
+# CORE IMPORTS
 import requests
-
-
 import os
+
+# THIRD PARTY IMPORTS
+from bs4 import BeautifulSoup
 
 class Parser:
     """
@@ -12,10 +13,13 @@ class Parser:
         self.startDate = 'NOW'
     
     def go(self, artist, source):
+        # INTERFACE WITH MAIN.PY
         self.artist = artist
         self.source = source
+
         if source.upper() == 'DISCOG':
             self.parseDiscog()
+            # TODO: SETUP OTHER PARSERS
         elif source.upper() == 'OTHER':
             self.parseOther()
 
@@ -23,17 +27,17 @@ class Parser:
         print(' -- FETCHING DISCOG: [' + self.artist + '] --')
         self.URL = 'https://www.discogs.com/search/?q=pink+floyd&type=master'
         DOM = self.pullDOM()
-
+        tags = [item for item in list(DOM.children) if type(item) != 'bs4.element.Tag']
+        print(len(tags))
     def parseOther(self):
+        # PLACHOLDER FOR OTHER SOURCE SITES
         self.URL = 'OTHER URL'
 
     def pullDOM(self):
+        # PULLS HTML PAGE INFO FROM URL
         page = requests.get(self.URL)
         if page.status_code == 200:
             soup = BeautifulSoup(page.content, 'html.parser')
             return soup
         else:
             return None
-        # soup = BeautifulSoup(html,'html.parser')
-
-        

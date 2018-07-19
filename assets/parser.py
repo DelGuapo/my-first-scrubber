701a1e74dac444b448343a06032896168c8081b7
Nick <bitweaverllc@gmail.com>
2018-07-19 20:51:29 +0800

    adding album class, looping through remote and finding local diffs

--- assets/parser.py
+++ assets/parser.py
@@ -5,6 +5,18 @@ import os
 # THIRD PARTY IMPORTS
 from bs4 import BeautifulSoup
 
+class Album:
+    """
+        object 
+    """
+    def __init__(self):
+        self.name = ''
+        self.url = ''
+        self.about = ''
+        self.artist = ''
+        self.artistClean = ''
+        self.tracklist = []
+
 class Parser:
     """
     This class parses files on local directory
@@ -12,11 +24,12 @@ class Parser:
     def __init__(self):
         self.startDate = 'NOW'
     
-    def go(self, artist, source):
+    def prepare(self, artist, source):
         # INTERFACE WITH MAIN.PY
-        self.artist = artist
+        self.artistClean = artist
+        self.artist = self.putTheInFront(artist)
         self.source = source
-
+        
         if source.upper() == 'DISCOG':
             self.parseDiscog()
             # TODO: SETUP OTHER PARSERS
@@ -25,15 +38,42 @@ class Parser:
 
     def parseDiscog(self):
         print(' -- FETCHING DISCOG: [' + self.artist + '] --')
-        self.URL = 'https://www.discogs.com/search/?q=pink+floyd&type=master'
-        DOM = self.pullDOM()
-        tags = [item for item in list(DOM.children) if type(item) != 'bs4.element.Tag']
-        print(len(tags))
+        discogTitle = self.artist.replace(' ','+')
+        self.baseURL = 'https://www.discogs.com'
+        self.URL = 'https://www.discogs.com/search/?q=' + discogTitle + '&type=master'
+        soup = self.pullDOMSoup()
+        self.albums = []
+        links = soup.findAll("a", {"class": "search_result_title"})
+
+        for link in links:
+            album = Album()
+            album.name = link.get('title')
+            album.url = self.baseURL +  link.get('href')
+            album.artistClean = self.artistClean
+            album.artist = self.artist
+            # TODO: add TrackList logic.
+            self.albums.append(album)
+
     def parseOther(self):
         # PLACHOLDER FOR OTHER SOURCE SITES
         self.URL = 'OTHER URL'
 
-    def pullDOM(self):
+    def putTheInFront(self, artist):
+        # removes ', the' and puts it at the beginning for searches
+        tmp = artist.lower().replace(', ',',')
+        commaThe = tmp.lower().split(',the')
+        if len(commaThe) > 1:
+            return 'the ' + commaThe[0]
+        else:
+            return artist
+    
+    def putTheInBack(self,artist):
+        if artist.upper()[:4] == 'THE ':
+            return artist[4:] + ', The'
+        else:
+            return artist
+
+    def pullDOMSoup(self):
         # PULLS HTML PAGE INFO FROM URL
         page = requests.get(self.URL)
         if page.status_code == 200:

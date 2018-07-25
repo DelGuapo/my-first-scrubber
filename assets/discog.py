98e1d865e58379809580d1740b42672d1cdebc45
Nick <bitweaverllc@gmail.com>
2018-07-25 20:04:54 +0800

    splitting logic to find ID, then albums for discog.

--- assets/discog.py
+++ assets/discog.py
@@ -1,8 +1,10 @@
 # CORE Dependencies
 import sys
+import re
 
 # THIRD PARTY IMPORTS
 from bs4 import BeautifulSoup
+import numpy
 
 # APP Dependencies
 from .html import HTMLParser
@@ -17,28 +19,63 @@ class DiscogParser:
         self.baseURL = 'https://www.discogs.com'
         self.artistDir = None
         self.albums = []
+        self.errors = None
 
     def findDiscigAlbums(self):
-        print('test')
-
-    def findDiscogId(self):
-        """
-            First search with keyword 
-        """
-        print(' -- FETCHING DISCOG: [' + self.artist.name + '] --')
-        discogTitle = self.artist.name.replace(' ','+')
-        searchUrl = 'https://www.discogs.com/search/?q=' + discogTitle + '&type=master'
+        print(' -- FETCHING DISCOG [' + self.artist.name + '] --')
+        searchUrl = 'https://www.discogs.com/artist/' + self.artist.id
         
-        soup = HTMLParser().pullDOMSoup()
-        self.albums = []
-        links = soup.findAll("a", {"class": "search_result_title"})
+        soup = HTMLParser(searchUrl).pullDOMSoup()
+        if(soup == None):
+            self.throwError('Could not pull Info')
+            return False
+            
+        albumLinks = soup.find('table', {'id' : 'artist'}).find_all("tr")
 
-        for link in links:
+        for link in albumLinks:
             album = AlbumConfig()
             album.name = link.get('title')
             album.url = self.baseURL +  link.get('href')
-            album.artistDir = self.artist.artistDir
+            album.artistDir = self.artist.dir 
             album.artist = self.artist.name
             # TODO: add TrackList logic.
             self.albums.append(album)
+
+    def throwError(self,errorString):
+        if(self.errors == None):
+            self.errors = []
+        self.errors.append(errorString)
+
+    def findDiscogId(self):
+        """
+            If config is not provided (or there is not an ID in the config file),
+            Search the website with a generic keyword search. 
+        """
+        print(' -- IDENTIFYING DISCOG [' + self.artist.name + '] --')
+        discogTitle = self.artist.name.replace(' ','+')
+        searchUrl = 'https://www.discogs.com/search/?q=' + discogTitle + '&type=master'
         
+        soup = HTMLParser(searchUrl).pullDOMSoup()
+        if(soup == None):
+            self.throwError('Could not pull Info')
+            return False
+
+        self.albums = []
+
+        artistLinks = soup.findAll(href=re.compile('/artist/'))
+        foundIds = []
+        for link in artistLinks:
+            tmpContent = link.contents
+            tmpId = link.get('href').replace('/artist/','').split('-')[0]
+            # if tmpId.isnumeric():
+            tmpArtist = tmpContent[0].upper() if len(tmpContent) > 0 else ''
+            if self.artist.name.upper() == tmpArtist and tmpId.isnumeric():
+                foundIds.append(int(tmpId))
+        
+        arr = numpy.array(foundIds)
+        stdev = numpy.std(arr,0)
+        if(len(arr) > 0 and stdev == 0):
+            self.artist.id = foundIds[0]
+            return self.artist.id
+        else:
+            return None
